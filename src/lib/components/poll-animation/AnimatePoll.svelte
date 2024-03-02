cdc0000c66189694f2a9f375df1a222cf04ff09c
birdpump <birdpump@gmail.com>
2024-03-02 14:44:22 -0800

    update

---
+++ src/lib/components/poll-animation/AnimatePoll.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { currentPoll } from '$lib/Store.js';
+    import { onMount } from 'svelte';
+
+    import Bar from "./bar.svelte"
+
+    let visible = false;
+
+
+    let data = [
+        {username: "bob4", score: 88},
+        {username: "bob5", score: 56}
+    ];
+
+    currentPoll.update(n => data) //todo should be updated from api
+    onMount(async () => {
+        visible = true
+    });
+</script>
+
+
+<style>
+    .animate-poll-cont{
+        width: 80%;
+    }
+</style>
+
+<div class="animate-poll-cont">
+    {#if visible}
+        {#each $currentPoll as data}
+            <Bar value={data.score} user={data.username}/>
+        {/each}
+    {/if}
+
+</div>
+
+
+
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,26 @@
 <script>
     import '$lib/assets/global.css'
+
+
+    import AnimatePoll from "$lib/components/poll-animation/AnimatePoll.svelte";
     export let data;
+
+
 </script>
 
-<h1>{data.slug}</h1>
+<style>
+    .main-cont {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 10px;
+        width: 100%;
+    }
+</style>
+
+<div class="main-cont">
+    <h1>{data.slug}</h1>
+    <AnimatePoll/>
+</div>
+
+
