0fddf1aaef26cb20c6e2e301c12962f4d17d1540
eronk1 <jeongahkim8174@gmail.com>
2024-03-02 17:51:13 -0800

    added login and signup

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -36,30 +36,84 @@
 </script>
 <style>
     #login-parent{
-
+        background: var(--cont-background);
+        height: fit-content;
+        width: fit-content;
+        border-radius: 8px;
+        margin: auto;
+        padding: 1rem 2rem 2rem 2rem;
+        display: grid;
+        width: 22rem;
     }
-</style>
+    .login-header{
+        text-align: center;
+        font-size: 35px;
+        margin: 0;
+        padding: 1.2rem 0;
+    }
+    .input-parent{
+        display: grid;
+        grid-template-rows: auto auto;
+        margin-bottom: 0.8rem;
+    }
+    .input-parent label{
+        font-size: 1.2rem;
+        color: var(--text);
+        margin-bottom: 0.2rem;
+    }
+    .input-parent input{
+        font-size: 1.2rem;
+        padding: 0.4rem 0 0.4rem 0.8rem;
+        color: var(--text);
+        border-radius: 5px;
+        outline: none;
+        border: 3.5px var(--primary) solid;
+        background-color: #1a1b26;
+    }
+    #submit-button-button{
+        background: none;
+        background: var(--submit-button);
+        padding: 0.35rem 0.7rem;
+        font-size: 1.1rem;
+        border: 3px rgb(117, 223, 170) solid;
+        border-radius: 3px;
+        margin: auto;
+        width: 100%;
+        margin-top: 0;
+    }
+    .pro-form{
+        display: grid;
+        grid-template-rows: auto auto auto auto;
+        gap: 0.7rem;
+    }
+    .special-property-password{
+        margin-bottom: 0;
+    }
+    .register-content-p{
+        margin: 0 !important;
+    }
+  </style>
 <div id="login-parent">
-    <h2>Login</h2>
+    <h2 class="login-header">Register</h2>
     {#if errorMessage}
         <p>{errorMessage}</p>
     {/if}
-    <form on:submit|preventDefault={handleLogin}>
-        <div>
+    <form class="pro-form" on:submit|preventDefault={handleLogin}>
+        <div class="input-parent">
             <label>Username: </label>
             <input type="text" bind:value={username}/>
         </div>
-        <div>
+        <div class="input-parent">
             <label>Password: </label>
             <input type="password" bind:value={password}/>
         </div>
-        <div>
+        <div class="input-parent">
             <label>Confirm Password: </label>
             <input type="password" bind:value={confirmPassword}/>
         </div>
-        <p style="color: blue; margin-top: 20px;">
-            Don't have an account? <a href="/register">Register</a>
+        <p class="register-content-p" style="color: blue; margin-top: 20px;">
+            Have an account? <a href="/login">Login</a>
         </p>
-        <button type="submit" on:click={login}>Login</button>
+        <button id="submit-button-button" type="submit" on:click={login}>Register</button>
     </form>
 </div>
